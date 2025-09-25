5ab94962de1bb4964e3680c5ea36a31690ebe36f
MARQUESDINISJoaoGabriel <joao-gabriel.marques-dinis@efrei.net>
2025-09-25 14:55:55 +0200

    labels

--- MainForm.cs
+++ MainForm.cs
@@ -4,8 +4,23 @@ using System.Windows.Forms;
 
 namespace netlogger
 {
-    public partial class MainForm : Form
+    public class MainForm : Form
     {
+        private Label statusLabel;
+        private System.Windows.Forms.Timer stageTimer;
+        private int stageIndex = 0;
+
+        private string[] stages = new string[]
+        {
+            "Wiring...",
+            "Fetching modem...",
+            "Routing...",
+            "Initializing...",
+            "Connecting to network...",
+            "Tunneling...",
+            "You are now online."
+        };
+
         public MainForm()
         {
 
@@ -14,8 +29,35 @@ namespace netlogger
             this.StartPosition = FormStartPosition.CenterScreen;
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
             this.BackColor = Color.FromArgb(65, 65, 65);
-            this.ForeColor = Color.FromArgb(245, 245, 245);; 
-            this.Font = new Font("Courier New", 12);
+            this.ForeColor = Color.FromArgb(245, 245, 245); ;
+            this.Font = new Font("Courier New", 12, FontStyle.Bold);
+
+            statusLabel = new Label();
+            statusLabel.Text = "Starting...";
+            statusLabel.AutoSize = false;
+            statusLabel.TextAlign = ContentAlignment.MiddleCenter;
+            statusLabel.Dock = DockStyle.Bottom;
+            statusLabel.Height = 50;
+            this.Controls.Add(statusLabel);
+
+            stageTimer = new System.Windows.Forms.Timer();
+            stageTimer.Interval = 2000;
+            stageTimer.Tick += StageTimer_Tick;
+            stageTimer.Start();
+        }
+                
+        private void StageTimer_Tick(object? sender, EventArgs e)
+        {
+            if (stageIndex < stages.Length)
+            {
+                statusLabel.Text = stages[stageIndex];
+                stageIndex++;
+            }
+            else
+            {
+                stageTimer.Stop();
+                this.Text = "netlogger - Online";
+            }
         }
     }
 }
